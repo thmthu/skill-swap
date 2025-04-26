350808e9d71d0c58aec13d0c03adc10e10450870
Ly-Lynn <linhlnt.work@gmail.com>
2025-04-26 14:56:56 +0700

    feat: add Google Oauth

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,9 +3,13 @@ import { Schema, model } from 'mongoose';
 var User = new Schema({
     username: { type: String, required: true },
     avatar: { type: String, default: '' },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true, match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please provide a valid email."
+    ]},
     bio: { type: String, default: '' },
-    password: { type: String, required: true },
+    password: { type: String, required: false },
+    googleId: { type: String },
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now },
     skills: { type: [String], default: [] },
