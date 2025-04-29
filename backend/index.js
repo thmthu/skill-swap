ded3c4bb7f05880c3122f2d2b13193b78b9e7052
Diem Qui <huynhlediemqui.vn@gmail.com>
2025-04-29 22:39:26 +0700

    feat: update backend api

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
-import 'dotenv/config';
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import mongoose from 'mongoose';
-import { createClient } from 'redis';
-import { 
-    authRouters,
-    userRouters,
-    connectionRouters
-} from './routes/routes.js';
-import connectMongo from './config/mongo.js';
-import redisClient from './config/redis.js';
+import "dotenv/config";
+import express from "express";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import { createClient } from "redis";
+import {
+	authRouters,
+	userRouters,
+	connectionRouters,
+} from "./routes/routes.js";
+import connectMongo from "./config/mongo.js";
+import redisClient from "./config/redis.js";
 
 connectMongo();
 
@@ -20,12 +20,11 @@ app.use(cookieParser());
 
 // redisClient.connect();
 
-// app.use('/api', authRouters, userRouters, connectionRouters);
-app.use('/api/auth', authRouters);
-app.use('/api/users', userRouters);
-app.use('/api/connections', connectionRouters);
-
+app.use('/api', authRouters, userRouters, connectionRouters);
+app.use("/api/auth", authRouters);
+app.use("/api/users", userRouters);
+app.use("/api/connections", connectionRouters);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
--- backend/routes/connectionRoutes.js
+++ backend/routes/connectionRoutes.js
@@ -1,8 +1,23 @@
-import { Router } from 'express';
-import { createConnection, respondConnection } from '../controllers/connectionControllers.js';
-import { authMiddleware } from '../middlewares/authMiddlewares.js';
+import { Router } from "express";
+import {
+	createConnection,
+	respondConnection,
+	getAllConnections,
+	getAllReceivedConnections,
+	getAllSentConnections,
+	getRecentConnections,
+} from "../controllers/connectionControllers.js";
+import { authMiddleware } from "../middlewares/authMiddlewares.js";
 
 export const router = Router();
 
-router.post('/', authMiddleware, createConnection);
-router.patch('/respond', authMiddleware, respondConnection);
+router.post("/", authMiddleware, createConnection);
+router.patch("/respond", authMiddleware, respondConnection);
+router.get("/", authMiddleware, getAllConnections);
+// router.get("/received", authMiddleware, getAllReceivedConnections);
+router.get("/received", getAllReceivedConnections);
+router.get("/sent", authMiddleware, getAllSentConnections);
+// router.get("/recent", authMiddleware, getRecentConnections);
+router.get("/recent", getRecentConnections);
+
+export default router;
