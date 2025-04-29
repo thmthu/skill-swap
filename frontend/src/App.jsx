523658b402b7af0eba518f500d38629587734467
Ly-Lynn <linhlnt.work@gmail.com>
2025-04-29 23:39:13 +0700

    fix: resolve conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,14 @@ import { useRoutes } from "react-router-dom";
 import { navRoutes } from "@/routes/NavRoutes";
 import Navbar from "@/components/Navbar/Navbar";
 import { Toaster } from "react-hot-toast";
+import { useTheme } from "@/context/ThemeContext";
+
 function App() {
   const element = useRoutes(navRoutes);
+  const { theme } = useTheme();
 
   return (
+    
     <div className="min-h-screen w-screen overflow-x-hidden bg-bg-light flex flex-col">
       <Toaster/>
       <Navbar />
