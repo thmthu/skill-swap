13665eccf66827a091ce18331d5a4f14f9b52c71
Diem Qui <huynhlediemqui.vn@gmail.com>
2025-05-03 02:40:40 +0700

    feat: add connect button to user card in home page

--- backend/routes/connectionRoutes.js
+++ backend/routes/connectionRoutes.js
@@ -18,7 +18,7 @@ router.get("/", authMiddleware, getAllConnections);
 router.get("/received", authMiddleware, getAllReceivedConnections);
 router.get("/sent", authMiddleware, getAllSentConnections);
 router.get("/recent", authMiddleware, getRecentConnections);
-router.post("/create/:senderId/:receiverId", authMiddleware, createConnection);
+router.post("/create/:receiverId", authMiddleware, createConnection);
 router.delete("/withdraw/:connectionId", authMiddleware, withdrawRequest);
 router.post("/accept/:connectionId", authMiddleware, acceptRequest);
 router.post("/reject/:connectionId", authMiddleware, rejectRequest);
